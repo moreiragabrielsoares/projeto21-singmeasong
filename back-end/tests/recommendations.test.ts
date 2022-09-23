3c2676fad261ed9a74de9b32fbde74957b20dc01
Gabriel Moreira <moreira.gabrielsoares@gmail.com>
2022-09-23 14:50:52 -0300

    create 2 integration tests for route POST /recommendations/:id/upvote

--- back-end/tests/recommendations.test.ts
+++ back-end/tests/recommendations.test.ts
@@ -1,7 +1,10 @@
 import supertest from "supertest";
 import app from "../src/app";
 import { prisma } from "../src/database";
-import { createNewRecommendation } from "../tests/factories/recommendationFactory";
+import {
+  createNewRecommendation,
+  insertNewRecommendation,
+} from "../tests/factories/recommendationFactory";
 
 beforeEach(async () => {
   await prisma.$executeRaw`TRUNCATE TABLE recommendations RESTART IDENTITY;`;
@@ -46,6 +49,44 @@ describe("POST /recommendations", () => {
 
     expect(result.status).toEqual(422);
   });
+});
+
+describe("POST /recommendations/:id/upvote", () => {
+  it("given a valid id it should return 200 and the score should be incremented by 1", async () => {
+    const insertedNewRecommendation = await insertNewRecommendation();
+    const { id, score } = insertedNewRecommendation;
+
+    const result = await supertest(app).post(`/recommendations/${id}/upvote`);
+
+    const { score: updatedScore } = await prisma.recommendation.findUnique({
+      where: { id },
+    });
+
+    expect(result.status).toEqual(200);
+    expect(updatedScore).toBe(score + 1);
+  });
+
+  it("given an invalid id it should return 404", async () => {
+    const invalidId = -1;
+
+    const result = await supertest(app).post(
+      `/recommendations/${invalidId}/upvote`
+    );
+
+    expect(result.status).toEqual(404);
+  });
+});
+
+describe("POST /recommendations/:id/downvote", () => {
+  it.todo(
+    "given a valid id it should return 200 and the score should be decremented by 1"
+  );
+
+  it.todo("given an invalid id it should return 404");
+
+  it.todo(
+    "given a recommendation id that its score reaches a value less than -5 it should return 200 and the recommendation should be deleted"
+  );
 });
 
 afterAll(async () => {
